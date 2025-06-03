cbc0b0541772fbaa48db398af5e93f40c17987b6
Steffen Maass <steffen.maass@awi.de>
2025-06-03 15:44:30 +0200

    symbolic linearization

--- src/TriB/operators.jl
+++ src/TriB/operators.jl
@@ -11,12 +11,16 @@ const c̃ = @syms c̃₁::Int c̃₂::Int c̃₃::Int
 const v = @syms v₁::Int v₂::Int
 const ṽ = @syms ṽ₁::Int ṽ₂::Int
 
+const n⃗ec, n⃗ec̃, n⃗eĉ = @variables n⃗ec[1:2] n⃗ec̃[1:2] n⃗eĉ[1:2]
+const n⃗ev, n⃗eṽ, n⃗ev̂ = @variables n⃗ev[1:2] n⃗eṽ[1:2] n⃗ev̂[1:2]
+const d = Symbolics.variable(:d)
+
 function dot(a,b)
     substitute(a[1] * b[1] + a[2] * b[2], Dict([sqrt3 => √3]))
 end
 
-n⃗e = ([ sqrt3//2;  1//2], [-sqrt3//2;  1//2], Num[ 0 ;-1//1])
-t⃗e = ([-1//2;  sqrt3//2], [-1//2; -sqrt3//2], Num[ 1//1;  0]) # e⃗₃ × n⃗e
+const n⃗e = ([ sqrt3//2;  1//2], [-sqrt3//2;  1//2], Num[ 0 ;-1//1])
+const t⃗e = ([-1//2;  sqrt3//2], [-1//2; -sqrt3//2], Num[ 1//1;  0]) # e⃗₃ × n⃗e
 
 _lê(le) = 2//3 * sqrt3//2 * le
 
@@ -93,7 +97,7 @@ end
 
 
 function ∇cv(_c, _v, p)
-    ve = ∑(_v, VE(e), p // 2)
+    ve = ∑((_v, n⃗ev), VE(e), p // 2)
     [ 1//Ac * ∑((e, n⃗ec), EC(_c), ve * le * n⃗ec[iTH] ) for iTH = 1:2]
 end
 
@@ -105,14 +109,14 @@ end
 # end
 
 function ∇cc(_cout, _cin, F⃗)
-    dF = [ ∑((_cin, n⃗ec̃), CE(e), (n⃗ec' * n⃗ec) * F[jTH]) for jTH=1:2 ]
+    dF = [ ∑((_cin, n⃗ec̃), CE(e), dot(n⃗ec, n⃗ec) * F⃗[jTH]) for jTH=1:2 ]
     [∑((e, n⃗ec), EC(_cout), n⃗ec[iTH] * dF[jTH]) for iTH=1:2, jTH=1:2]
 end
 
 # divergence operators
 
 function ∇ᵀvc(_v, _c, F⃗)
-    fe = ∑((_c, n⃗ec), CE(e), lê//2 * n⃗ev' * F⃗)
+    fe = ∑((_c, n⃗ec), CE(e), lê//2 * dot(n⃗ev, F⃗))
     1//Av * ∑((e, n⃗ev), EV(_v), fe)
 end
 
@@ -148,9 +152,9 @@ end
 # end
 
 function u⃗ᵀ∇(_cout, _cin, u⃗tr, u⃗ad) # c ≡ _cout, ĉ ≡ _cin
-    F = ∑((_cin, n⃗eĉ), CE(e), le//2 * n⃗ec̃' * u⃗ad)
-    Ru = [evalat(c̃, _cin, u⃗tr[iTH]) + le//2 * n⃗ec̃' * ∇(c̃, _cin, u⃗tr[iTH]) for iTH=1:2]
-    Fu = ∑((c̃, n⃗ec̃), CE(e), n⃗ec' * n⃗ec̃ * 0.5*(F + abs(F)) * ( Ru[iTH] - evalat(_cout, _cin, u⃗tr[iTH]) )) 
+    F = ∑((_cin, n⃗eĉ), CE(e), le//2 * dot(n⃗ec̃, u⃗ad))
+    Ru = [evalat(c̃, _cin, u⃗tr[iTH]) + le//2 * dot(n⃗ec̃, ∇(c̃, _cin, u⃗tr[iTH])) for iTH=1:2]
+    Fu = ∑((c̃, n⃗ec̃), CE(e), dot(n⃗ec, n⃗ec̃) * 0.5*(F + abs(F)) * ( Ru[iTH] - evalat(_cout, _cin, u⃗tr[iTH]) )) 
     [1//Ac * ∑((e, n⃗ec), EC(_cout), Fu) for iTH=1:2]
 end
 
@@ -177,18 +181,21 @@ end
 # end
 
 function ∇ᵀ(_vout, _c, _vin, u⃗, b; γ=3//4) # v ≡ _vout
-    F = ∑((_c, n⃗ec), CE(e), lê//2 * n⃗ev' * u⃗)
-    ∇beᶜ = 1//le * ∑((_vin, n⃗ev̂), VE(e), n⃗eṽ' * n⃗ev̂ * b)
+    F = ∑((_c, n⃗ec), CE(e), lê//2 * dot(n⃗ev, u⃗))
+    ∇beᶜ = 1//le * ∑((_vin, n⃗ev̂), VE(e), dot(n⃗eṽ, n⃗ev̂) * b)
     #∇beᵘ = n⃗eṽ' * ∇cv(cup(ṽ, n⃗eṽ), _vin, b)
-    ∇beᶜ = 1//le * ∑((_vin, d), VEce(e,n⃗ev), d * b)
+    ∇beᵘ = 1//le * ∑((_vin, d), VEup(e,n⃗ev), d * b)
     ∇be  = 2//3 * ∇beᶜ + 1//3 * ∇beᵘ
     Rbe  = evalat(ṽ, _vin, b) + le//2 * ∇be
-    1//Av * ∑((e, n⃗ev), EV(_vout), ∑((ṽ, n⃗eṽ), VE(e), n⃗ev' * n⃗eṽ * (F + (1-γ) * abs(F))/2 * Rbe))
+    1//Av * ∑((e, n⃗ev), EV(_vout), ∑((ṽ, n⃗eṽ), VE(e), dot(n⃗ev, n⃗eṽ) * (F + (1-γ) * abs(F))/2 * Rbe))
 end
 
 const nS = 3
 
+@register_symbolic ∫dz(x)
+
 function bous_sys()
+    @variables f₀
     @variables t z
     @variables (u(t,z))[1:2,-nS:nS,-nS:nS,-nS:nS] (b(t,z))[-nS:nS,-nS:nS] (η(t))[-nS:nS, -nS:nS]
     @variables (w(t,z))[-nS:nS, -nS:nS] (p(t,z))[-nS:nS, -nS:nS]
@@ -199,16 +206,88 @@ function bous_sys()
     ∂ₜ = Differential(t)
     ∂₃ = Differential(z)
 
-    u⃗ = [u[iTH, c[1], c[2], c[3]] for iTH=1:2]
-    b = b[v[1], v[2]]
-    η = η[v[1], v[2]]
+    u⃗ = [u[iTH, cin[1], cin[2], cin[3]] for iTH=1:2]
+    b = b[vin[1], vin[2]]
+    η = η[vin[1], vin[2]]
+    w = w[vin[1], vin[2]]
+    p = p[vin[1], vin[2]]
     
 
     eqs = [
         ∇ᵀvc(vout, cin, u⃗) + ∂₃(evalat(vout, vin, w)) ~ 0,
-        ∂ₜ.(evalat(cout, cin, u⃗)) + ∇ᵀcc(cout, cin, u⃗, u⃗) + ∂₃(evalat(cout, cin, u⃗) .* av_cv(c̃, v, w)) + f₀ * [0 -1; 1 0] * evalat(cout, cin, u⃗) + ∇cv(cout, vin, p) + ∇cv(cout, vin, η) .~ 0,
+        ∂ₜ.(evalat(cout, cin, u⃗)) .+ ∇ᵀcc(cout, cin, u⃗, u⃗) .+ ∂₃.(evalat(cout, cin, u⃗) .* av_cv(cout, vin, w)) .+ f₀ * [0 -1; 1 0] * evalat(cout, cin, u⃗) .~ -∇cv(cout, vin, p) .- ∇cv(cout, vin, η),
         ∂₃(evalat(vout, vin, p)) - evalat(vout, vin, b) ~ 0, 
         ∂ₜ(evalat(vout, vin, b)) + ∇ᵀ(vout, cin, vin, u⃗, b) + ∂₃(evalat(vout, vin, w * b)) ~ 0,
-        ∂ₜ(evalat(vout, vin, η)) + ∇ᵀvc(vout, cin, ∫dz.(u⃗)) ~ 0,
+        ∂ₜ(evalat(vout, vin, η)) + ∇ᵀvc(vout, cin, ∫dz.(u⃗)) ~ 0
     ]
+    eqs = evalat((0,0), vout, eqs)
+    eqs = evalat((0,0,1), cout, eqs)
+end
+
+function linearized_bous_sys()
+    @variables f₀ M2
+    @variables t z
+    @variables (du(t,z))[1:2,-nS:nS,-nS:nS,1:2] (db(t,z))[-nS:nS,-nS:nS] (dη(t))[-nS:nS, -nS:nS]
+    @variables (dw(t,z))[-nS:nS, -nS:nS] (dp(t,z))[-nS:nS, -nS:nS]
+    vin  = @syms vin₁::Int vin₂::Int
+    vout = @syms vout₁::Int vout₂::Int
+    cin  = @syms cin₁::Int cin₂::Int cin₃::Int
+    cout = @syms cout₁::Int cout₂::Int cout₃::Int
+    ∂ₜ = Differential(t)
+    ∂₃ = Differential(z)
+
+    @variables ū v̄ ϵ
+    db1 = M2 * sqrt3//2
+    b̄ = OffsetArray([x * db1 for x=-nS:nS, y=-nS:nS], -nS:nS, -nS:nS)
+    u⃗̄ = [1; 0] .* ones(1:1, -nS:nS, -nS:nS, 1:2)
+    
+    u⃗ = [u⃗̄[iTH, cin[1], cin[2], cin[3]] + ϵ * du[iTH, cin[1], cin[2], cin[3]] for iTH=1:2]
+    b = b̄[vin[1], vin[2]] + ϵ * db[vin[1], vin[2]]
+    η = ϵ * dη[vin[1], vin[2]]
+    w = ϵ * dw[vin[1], vin[2]]
+    p = ϵ * dp[vin[1], vin[2]]
+    
+
+    eqs = [
+        collect(∂ₜ.(evalat(cout, cin, u⃗)) .+ ∇ᵀcc(cout, cin, u⃗, u⃗) .+ ∂₃.(evalat(cout, cin, u⃗) .* av_cv(cout, vin, w)) .+ f₀ * [0 -1; 1 0] * evalat(cout, cin, u⃗) .~ -∇cv(cout, vin, p) .- ∇cv(cout, vin, η))...,
+        ∇ᵀvc(vout, cin, u⃗) + ∂₃(evalat(vout, vin, w)) ~ 0,
+        ∂₃(evalat(vout, vin, p)) - evalat(vout, vin, b) ~ 0, 
+        ∂ₜ(evalat(vout, vin, b)) + ∇ᵀ(vout, cin, vin, u⃗, b) + ∂₃(evalat(vout, vin, w * b)) ~ 0,
+        #∂ₜ(evalat(vout, vin, η)) + ∇ᵀvc(vout, cin, ∫dz.(u⃗)) ~ 0
+    ]
+    eqs = [reduce(vcat, [evalat((0,0,iHc), cout, eqs[iTH]) for iHc=1:2,iTH=1:2]); [evalat((0,0), vout, eq) for eq in eqs[3:end]]]
+
+    eqs = [Symbolics.taylor_coeff(simplify(expand_derivatives(eq.lhs)), ϵ, 1) ~ Symbolics.taylor_coeff(simplify(expand_derivatives(eq.rhs)), ϵ, 1) for eq in eqs]
+
+    @variables k l
+    @variables (û(t,z))[1:2] (v̂(t,z))[1:2] b̂(t,z) η̂(t)
+    @variables ŵ(t,z) p̂(t,z)
+    u⃗̂ = [û v̂]
+    ϕ = exp.(im * [k * x + l * y for x = -nS:nS, y = -nS:nS])
+    ϕcv = [exp(im * (k * 1//3 + l * -2//3)), exp(im * (k * -1//3 + l * -1//3))]
+    ϕvc = [exp(im * (k * -1//3 + l * 2//3)), exp(im * (k * 1//3 + l * 1//3))]
+    ϕcc = [1                                exp(im * (k * 0 + l * -1//sqrt3));
+             exp(im * (k * 0 + l * 1//sqrt3)) 1]
+
+    subs_c = [
+        merge(
+            Dict(Symbolics.scalarize(du) .=> real([ϕ[x,y] * ϕcc[iHc,jHc] * u⃗̂[jTH,jHc] for jTH=1:2,x=1:size(ϕ,1),y=1:size(ϕ,2),jHc=1:2])),
+            Dict(Symbolics.scalarize(db) .=> real(ϕ * ϕcv[iHc] * b̂)),
+            Dict(Symbolics.scalarize(dη) .=> real(ϕ * ϕcv[iHc] * η̂)),
+            Dict(Symbolics.scalarize(dp) .=> real(ϕ * ϕcv[iHc] * p̂)),
+            Dict(Symbolics.scalarize(dw) .=> real(ϕ * ϕcv[iHc] * ŵ)),
+        )
+        for iHc = 1:2]
+    subs_v = merge(
+            Dict(Symbolics.scalarize(du) .=> real([ϕ[x,y] * ϕvc[jHc] * u⃗̂[jTH,jHc] for jTH=1:2,x=1:size(ϕ,1),y=1:size(ϕ,2),jHc=1:2])),
+            Dict(Symbolics.scalarize(db) .=> real(ϕ  * b̂)),
+            Dict(Symbolics.scalarize(dη) .=> real(ϕ  * η̂)),
+            Dict(Symbolics.scalarize(dp) .=> real(ϕ  * p̂)),
+            Dict(Symbolics.scalarize(dw) .=> real(ϕ  * ŵ)),
+        )
+
+    eqs = [[substitute(eq, subs_c[1]) for eq in eqs[1:2]]; [substitute(eq, subs_c[2]) for eq in eqs[3:4]]; [substitute(eq, subs_v) for eq in eqs[5:end]]]
+    
+    # eqs = [simplify(substitute(eqs[1], subs_v); expand=true), substitute(eqs[2], subs_c[1]), substitute(eqs[3], subs_c[1]), substitute(eqs[4], subs_v), substitute(eqs[5], subs_v)]
+    eqs
 end
